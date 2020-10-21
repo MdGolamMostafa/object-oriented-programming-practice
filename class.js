93c4cf61bd0ee96166d532b6124b2a7ecb710edc
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-21 11:43:38 +0600

    normal class based example

---
+++ class.js
@@ -0,0 +1,18 @@
+class Mouse{
+    constructor(color,price,quantity) {
+        this.color = color;
+        this.price = price;
+        this.quantity = quantity
+    }
+}
+const ashiqMouse = new Mouse('black',150,2);
+console.log(ashiqMouse);
+
+function Jug(color,price,capacity)  {
+    this.color = color;
+    this.price = price;
+    this.capacity = capacity
+}
+
+const ashiqJug = new Jug('white',132,3);
+console.log(ashiqJug);
