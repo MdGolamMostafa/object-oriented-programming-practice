1d85b571af6e2f801b29c3b4b27f7ac1303260c9
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-22 09:05:38 +0600

    encapsulation example

---
+++ encapsulation.js
@@ -0,0 +1,15 @@
+class Tiger{
+    #region = "haka";
+    constructor(name){
+        this.name = name;
+        this.#region = "region";
+    }
+    run(){
+        console.log(`I am ${this.name} speed style is -- ${this.#region}`)
+    }
+}
+const animals = new Tiger('nakal');
+animals.run();
+// animals.#region = "haka"
+animals.name ='malam';
+console.log(animals);
